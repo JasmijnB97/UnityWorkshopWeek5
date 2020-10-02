6b609e1a4920e16e80dab6e04c1d0c86b9018e1f
R0vi <ivorstaatsl0l@gmail.com>
2020-10-02 14:00:13 +0200

    added zombie with navmesh agent

---
+++ UnityWorkshopWeek5/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public Transform player;
+    public float detectionRange;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, player.position) <= detectionRange)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
